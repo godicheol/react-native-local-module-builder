0645aaf878b65971e0caa071009473049a270b72
godicheol <godicheol@gmail.com>
2023-02-08 07:36:02 +0900

    init

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function MyComponent() {
+    return <h1>Hello World</h1>
+}
+
+export default MyComponent;
